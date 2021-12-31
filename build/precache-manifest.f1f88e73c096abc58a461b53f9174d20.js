0ff04e7680f45ea6bbe3b48f21920bfe6b5dc5d0
tetsis <hitoshi.zin.nakao@gmail.com>
2021-12-31 11:54:24 +0900

    add build foloder

---
+++ build/precache-manifest.f1f88e73c096abc58a461b53f9174d20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd343923246c6a4d6ca1",
+    "url": "/static/css/main.3c6f697d.chunk.css"
+  },
+  {
+    "revision": "bd343923246c6a4d6ca1",
+    "url": "/static/js/main.bd343923.chunk.js"
+  },
+  {
+    "revision": "5d18c4d22593e08bb205",
+    "url": "/static/js/1.5d18c4d2.chunk.js"
+  },
+  {
+    "revision": "dfebe0cbfb78e3ebde11",
+    "url": "/static/css/2.055b4ceb.chunk.css"
+  },
+  {
+    "revision": "dfebe0cbfb78e3ebde11",
+    "url": "/static/js/2.dfebe0cb.chunk.js"
+  },
+  {
+    "revision": "f31364dab76a772d7234",
+    "url": "/static/js/runtime~main.f31364da.js"
+  },
+  {
+    "revision": "859076cf002fcf3c071b7ca89fae02ff",
+    "url": "/index.html"
+  }
+];
